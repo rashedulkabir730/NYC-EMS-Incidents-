2f3fdbce2978df8f98476f83dc1508edb710d07e
Rashedul Kabir <rashedulkabir107@gmail.com>
2026-02-07 09:38:54 -0500

    renamed and removed stuff

--- text.py
+++
@@ -1,8 +0,0 @@
-import duckdb
-
-
-con = duckdb.connect("data/raw.duckdb")
-#con.execute("DROP TABLE IF EXISTS raw_api_data")
-#print(con.execute("PRAGMA table_info('raw_api_data')").fetchall())
-
-print(con.execute("SHOW TABLES").fetchall())
